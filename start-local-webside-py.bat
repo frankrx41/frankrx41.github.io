509bcc67c40ce1cc3cbdfe9cb30023275d3b6a5b
frankrx41 <frankrx41@gmail.com>
2022-06-27 06:00:25 +0800

    First commit
    * Add local website start script
    * Add favicon.ico
    - Add normalize.css

---
+++ start-local-webside-py.bat
@@ -0,0 +1,15 @@
+@echo off
+echo.This program starts a local server
+echo.Then open your default browser and navigate to this folder of websites
+echo.The server will be terminated when the window is closed
+
+@REM set web_port=%random%
+set web_port=4096
+
+echo.
+echo.Http: 127.0.0.1
+echo.Port: %web_port%
+
+echo.
+start http://127.0.0.1:%web_port%/
+py -m http.server %web_port%
